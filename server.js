c40b1b0cdc0e56f7082fe99d5a0a943ce898aadb
danny-g04 <guodanny04@gmail.com>
2025-12-10 20:50:55 -0800

    idk

--- server.js
+++ server.js
@@ -44,8 +44,19 @@ app.get('/', (req, res) => {
   res.send('Server is running!');
 });
 
-// Actually query the database
+//function to query db
+function query(sql, param) {
+  return new Promise((resolve, reject) => {
+    db.query(sql, param, (err, result) => {
+      if (err)
+        return reject(err);
+      resolve(result)
+    })
+  });
+};
 
+//------------- routes for index.js --------------- \\
+  //adds info to table in db
 app.post('/faculty', (req, res) => {
   const { name, role, salary_id } = req.body;
   if (!name) return res.status(400).json({ error: 'Name is required' });
@@ -93,17 +104,105 @@ app.post('/students', (req, res) => {
   }
 });
 
-//function to query db
-function query(sql, param) {
-  return new Promise((resolve, reject) => {
-    db.query(sql, param, (err, result) => {
-      if (err)
-        return reject(err);
-      resolve(result)
-    })
-  });
-};
+app.post('/save-draft', (req, res) => {
+  const { title, budget_cost, yearsSelect, facultyIDs, studentIDs } = req.body;
 
+  // get budget stuff. 
+  db.query(
+    'INSERT INTO budgets (title, total_amount, length) VALUES (?,?, ?)',
+    [title, budget_cost, yearsSelect],
+    (err, result) => {
+      if (err) return res.status(500).json({ error: err.message });
+
+      const budget_id = result.insertId;
+
+      // fill the members table for faculty member
+      for (let i = 0; i < facultyIDs.length; i++) {
+        const fid = facultyIDs[i];
+        db.query(
+          'INSERT INTO members (user_id, budget_id, member_type, people_id) VALUES (?, ?, "faculty", ?)',
+          [req.session.user_id, budget_id, fid]
+        );
+      }
+
+      // fills the members table for students
+      for (let i = 0; i < studentIDs.length; i++) {
+        const sid = studentIDs[i];
+        db.query(
+          'INSERT INTO members (user_id, budget_id, member_type, people_id) VALUES (?, ?, "student", ?)',
+          [req.session.user_id, budget_id, sid]
+        );
+      }
+      res.json({ ok: true, budget_id });
+    }
+  );
+});
+
+  //----Queries from db ----\\
+app.get('/travel-profiles', (req, res) => {
+  const sql = `
+    SELECT id, 
+      trip_type, 
+      airfare, 
+      per_diem, 
+      lodging_caps 
+    FROM travel_profiles 
+    ORDER BY id
+    `;
+  db.query(sql, (err, rows) => {
+    res.json(rows);
+  });
+});
+
+app.get('/tuition', (req, res) => {
+  const { semester, residency } = req.query;
+
+  const sql = `
+    SELECT tuition_semester,fee_semester 
+    FROM tuition_fee_schedules 
+    Where semester = ? AND residency_status = ? 
+    `;
+  db.query(sql, [semester, residency], (err, rows) => {
+    res.json(rows[0]);
+  });
+});
+
+app.get('/students', (req, res) => {
+  const sql = `
+      SELECT s.student_id,
+             s.name,
+             s.residency_status,
+             s.salary_id,
+             sal.salary,         
+             sal.fringe_rate    
+      FROM students s
+      LEFT JOIN salary sal ON sal.salary_id = s.salary_id
+      ORDER BY s.student_id
+    `;
+  db.query(sql, (err, rows) => {
+    res.json(rows); // do I really need the json will dive deeper into this as we progress
+  });
+});
+
+// Joins faculty with salary id
+app.get('/faculty', (req, res) => {
+  const sql = `
+      SELECT f.faculty_id,
+             f.name,
+             f.role,
+             s.salary,
+             s.fringe_rate
+      FROM faculty f
+      JOIN salary s ON s.salary_id = f.salary_id
+      ORDER BY f.name
+    `;
+  db.query(sql, (err, rows) => {
+    res.json(rows);
+  });
+});
+
+
+//------------- routes for login/register.js --------------- \\
 //adds users to db when they register
 app.post('/register', async (req, res) => {     //async allows for await to be used since queries and hashing takes time
   const { name, email, password, passwordConfirm } = req.body;   //stores the info from frontend
@@ -177,7 +276,6 @@ app.get('/session', (req, res) => {
     res.json({ loggedIn: false });
 });
 
-
 //destroy session when user logs out
 app.post('/logout', (req, res) => {
   req.session.destroy(err => {
@@ -190,108 +288,13 @@ app.post('/logout', (req, res) => {
   });
 });
 
-// Joins salary with the students table
-app.get('/students', (req, res) => {
-  const sql = `
-      SELECT s.student_id,
-             s.name,
-             s.residency_status,
-             s.salary_id,
-             sal.salary,         
-             sal.fringe_rate    
-      FROM students s
-      LEFT JOIN salary sal ON sal.salary_id = s.salary_id
-      ORDER BY s.student_id
-    `;
-  db.query(sql, (err, rows) => {
-    res.json(rows); // do I really need the json will dive deeper into this as we progress
-  });
+
+//------------- routes for grant.js --------------- \\
+
+app.get('grant', (req, res)=>{
+  let sql =` SELECT from `
+  ;
 });
-
-// Joins faculty with salary id
-app.get('/faculty', (req, res) => {
-  const sql = `
-      SELECT f.faculty_id,
-             f.name,
-             f.role,
-             s.salary,
-             s.fringe_rate
-      FROM faculty f
-      JOIN salary s ON s.salary_id = f.salary_id
-      ORDER BY f.name
-    `;
-  db.query(sql, (err, rows) => {
-    res.json(rows);
-  });
-});
-
-// Travel Profiles
-app.get('/travel-profiles', (req, res) => {
-  const sql = `
-    SELECT id, 
-      trip_type, 
-      airfare, 
-      per_diem, 
-      lodging_caps 
-    FROM travel_profiles 
-    ORDER BY id
-    `;
-  db.query(sql, (err, rows) => {
-    res.json(rows);
-  });
-});
-
-app.get('/tuition', (req, res) => {
-  const { semester, residency } = req.query;
-
-  const sql = `
-    SELECT tuition_semester,fee_semester 
-    FROM tuition_fee_schedules 
-    Where semester = ? AND residency_status = ? 
-    `;
-  db.query(sql, [semester, residency], (err, rows) => {
-    res.json(rows[0]);
-  });
-});
-
-
-// Have to have the members and budget be added to database at the end because we dont get the budget id until the last step
-app.post('/save-draft', (req, res) => {
-  const { title, budget_cost, facultyIDs, studentIDs } = req.body;
-
-  // get budget stuff. 
-  db.query(
-    'INSERT INTO budgets (title, total_amount) VALUES (?,?)',
-    [title, budget_cost],
-    (err, result) => {
-      if (err) return res.status(500).json({ error: err.message });
-
-      const budget_id = result.insertId;
-
-      // fill the members table for faculty member
-      for (let i = 0; i < facultyIDs.length; i++) {
-        const fid = facultyIDs[i];
-        db.query(
-          'INSERT INTO members (user_id, budget_id, member_type, people_id) VALUES (?, ?, "faculty", ?)',
-          [req.session.user_id, budget_id, fid]
-        );
-      }
-
-      // fills the members table for students
-      for (let i = 0; i < studentIDs.length; i++) {
-        const sid = studentIDs[i];
-        db.query(
-          'INSERT INTO members (user_id, budget_id, member_type, people_id) VALUES (?, ?, "student", ?)',
-          [req.session.user_id, budget_id, sid]
-        );
-      }
-
-      // 
-      res.json({ ok: true, budget_id });
-    }
-  );
-});
-
 
 // Start the server
 const PORT = 3000;
